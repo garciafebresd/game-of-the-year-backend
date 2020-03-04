2838e9c215df0e3841ff682311874159670373cf
Tecnico2011 <garciafebresd@gmail.com>
2020-03-03 23:57:54 -0300

    google functions development

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,18 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://game-of-the-year-88c0f.firebaseio.com"
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.json("Hello desde funciones Firebase!");
+    response.json({
+        message: "Hello desde funciones Firebase!"
+    });
 });
