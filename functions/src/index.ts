2c6c33c2c88dacea781e0a05784ef9ad79553737
daniel garcia <dgarcia@position.cl>
2020-03-04 18:56:04 -0300

    backend done. working in firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,8 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
+
 
 const serviceAccount = require("./serviceAccountKey.json");
 
@@ -8,11 +11,66 @@ admin.initializeApp({
     databaseURL: "https://game-of-the-year-88c0f.firebaseio.com"
 });
 
+const database = admin.firestore();
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//     response.json({
+//         message: "Hello desde funciones Firebase!"
+//     });
+// });
+//
+// export const getGOTY = functions.https.onRequest( async (request, response) => {
+//     // const nombre = request.query.nombre || 'Sin nombre';
+//     // response.json({nombre});
+//     const gotyRef = database.collection('goty');
+//     const docsSnap = await gotyRef.get();
+//     const games = docsSnap.docs.map(doc => doc.data());
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+//     response.json(games);
+// });
+
+
+//EXPRESS
+const app = express();
+app.use(cors({ origin: true }));
+
+//GET
+app.get('/goty', async (request, response) => {
+    const gotyRef = database.collection('goty');
+    const docsSnap = await gotyRef.get();
+    const games = docsSnap.docs.map(doc => doc.data());
+
+    response.json(games);
+});
+
+//POST
+app.post('/goty/:id', async (request, response) => {
+
+    const id = request.params.id;
+    const gameRef = database.collection('goty').doc(id);
+    const gameSnap = await gameRef.get();
+
+    if (!gameSnap.exists) {
+        response.status(404).json({
+            ok: false,
+            message: 'No existe un juego con el id => ' + id
+        });
+    }
+
+    const currentGameData = gameSnap.data() || { votos: 0 };
+    await gameRef.update({
+        votos: currentGameData.votos + 1
+    });
+
     response.json({
-        message: "Hello desde funciones Firebase!"
+        ok: true,
+        message: `Gracias por tu voto a ${currentGameData.name}`
     });
 });
+
+
+// exports.api = functions.https.onRequest(app);
+export const api = functions.https.onRequest(app);
